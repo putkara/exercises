f37e0053acb07832b45f9c4a8a8d91732cc13919
putkara <natia.meskhi.natia@gmail.com>
2020-05-14 16:12:00 +0200

    natia solution

---
+++ 04-PB-operators-arithmetic-pt2/natia.js
@@ -0,0 +1,60 @@
+//Basic Arithmetic Problems
+//1
+let resultOne = 6450 / 15;
+console.log(resultOne);
+//2
+let year = 500 * 52;
+console.log(year);
+//Percentage
+//3
+let result = (17 / 30) * 100;
+console.log(result + '%');
+
+//Geometry Formulas
+
+//4
+let square = 4.75 * 4;
+console.log(square);
+
+//5
+let triangle = 5 + 6 + 7;
+console.log(triangle);
+//6
+
+let area = 5 * 5;
+console.log(area);
+
+//7
+let hero = (5 + 6 + 7) / 2;
+console.log(hero);
+let HeronsFormula = Math.sqrt(hero * (hero-5)*(hero-6)*(hero-7) );
+console.log(HeronsFormula);
+
+//8
+let volume = 9 * 9 * 9;
+console.log(volume);
+
+//Consumer Formula
+
+//9
+let tip = ((22.35 * 100) / 10);
+console.log(tip + '€');
+let tipOne = ((26.67 * 100) / 15);
+console.log(tipOne + '€');
+let tipTwo = ((35.92 * 100) / 20);
+console.log(tipTwo + '€');
+
+//Average
+
+//10
+let noemy = (8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4)/14;
+console.log(noemy + " " + 'hours');
+
+// 11
+//12
+
+
+// i dont anderstand 
+
+
+ 
