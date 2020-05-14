b481ce4ac5b94387e0eca23621635e9a71d1c98b
putkara <natia.meskhi.natia@gmail.com>
2020-05-14 13:49:45 +0200

    natia

---
+++ 03-PB-console/03console.js
@@ -0,0 +1,12 @@
+
+name = 'John';
+lastName = 'Smith';
+freeSpace = ' ';
+let city = 'Berlin';
+let ege = '43';
+let job = 'Teacher';
+console.log(name, freeSpace, lastName);
+console.log('City' + freeSpace + city);
+console.log('Ege'+ freeSpace + ege);
+console.log('Job'+ freeSpace + job);
+console.log(name + ' ' + lastName + ' is a ' + ege + ' old ' + ' ' + job + ' who lives in ' + city);
