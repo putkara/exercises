bfeda1544ba80c063f1141f217f0fc06d184a4ec
putkara <natia.meskhi.natia@gmail.com>
2020-05-19 20:18:05 +0200

    02 Natia

---
+++ 02-PB-operators-arithmetic-pt1/02natia.js
@@ -0,0 +1,41 @@
+let x = 6;
+let y = 8;
+
+//1
+console.log(x + y);
+
+//02
+console.log(y - x);
+
+//03
+console.log(x * y);
+
+//4
+console.log(x / y);
+
+//5
+let z = 10;
+let resultOne = x * y * z;
+console.log(resultOne);
+
+//5,1
+let a = 15;
+let b = 9;
+
+//6
+let c = 20;
+let resultTwo = (a + b) * c;
+console.log(resultTwo);
+
+//7
+console.log( ++ a);
+
+//8
+console.log( -- b);
+
+//9
+let d = a + b;
+console.log(d + c);
+
+// 10.
+console.log(resultOne / resultTwo);
