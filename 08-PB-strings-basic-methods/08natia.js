046f5101df26197e351a0c6d8420a1212e790ec7
putkara <natia.meskhi.natia@gmail.com>
2020-05-19 14:31:55 +0200

    natia

---
+++ 08-PB-strings-basic-methods/08natia.js
@@ -0,0 +1,51 @@
+
+//1
+
+let park = 'I can walk in the park  all day!';
+console.log(park.substring(18, 22));
+   
+//2
+ let helloWorld = "Hello World";
+ helloWorld = helloWorld.toUpperCase();  
+ console.log(helloWorld);
+
+ //3
+ helloWorld = helloWorld.toLowerCase();
+ console.log(helloWorld); 
+
+ //4
+ let JavaScript = 'JavaScript';
+ console.log(JavaScript.substring(3, 6));
+
+ //5 
+let check = "nice shoes";
+console.log(check.includes("l"));
+console.log(check.includes("n"));
+
+//6
+let Bananas = 'Bananas';
+console.log(Bananas[0] + Bananas + Bananas[0]);
+
+//7
+let Scritch = 'Scritch';
+console.log(Scritch.substring(4, 7) + Scritch + Scritch.substring(4, 7));
+
+//8
+
+let output = 'BoogieWoogie';
+console.log( output.substring(5,6) + output.substring(1, 6) + output.substring( 6,11 )+output.substring( 0,1 ));
+
+//9
+let firstName = "Natia";
+let city = "Berlin";
+console.log(`My name is ${firstName}. I live in ${city} and I am a student.`);
+
+//10
+
+let fox = "the quick brown fox";
+fox = `${fox[0].toUpperCase()}${fox.substring(1)}`;
+
+console.log(fox);
+
+//10 or we can do
+console.log(fox[0].toUpperCase() + fox.slice(1));
