89cb52b25d68b69ca24bc61c69419e886dc9f1e8
putkara <natia.meskhi.natia@gmail.com>
2020-05-19 15:49:16 +0200

    natia

---
+++ 05-PB-operators-comparison/05natia.js
@@ -0,0 +1,37 @@
+
+//0
+let x = 20;
+let y = 30;
+
+//1
+console.log(x == y);
+
+//2
+console.log( x !== y);
+
+//03
+console.log( x > y);
+
+
+//04
+console.log( x <= y);
+
+//05
+console.log( x < y);
+
+//06
+let z = 5;
+console.log((z * x) > (z + y));
+
+//07
+console.log((x - z) < (y / z));
+
+//08
+console.log((x === y) ? true : false) ;
+console.log((y === z) ? true : false) ;
+console.log((x === z) ? true : false) ;
+//09
+console.log((x / z) === (y / z));
+
+
+
