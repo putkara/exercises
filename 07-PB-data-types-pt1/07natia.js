1ee0957f64e0c7c20f700327bcd6fef2135eef4d
putkara <natia.meskhi.natia@gmail.com>
2020-05-19 17:40:03 +0200

    natia

---
+++ 07-PB-data-types-pt1/07natia.js
@@ -0,0 +1,37 @@
+// 1 
+let string = "The string is at least 6 characters long!";
+console.log( string);
+
+//2
+console.log( string[1] + string[5]);
+
+//3
+let name = 'Natia';
+
+//4
+let age = 33 ;
+
+//5
+let year = 2020;
+
+//6
+console.log(`${name} is ${age} in ${year}`);
+
+
+//7
+let isMarried = true;
+
+//8
+console.log(`${name} ist married: ${isMarried}`);
+
+//9
+let programmingLanguage ="JavaScript";
+let isFun = true;
+console.log(`${programmingLanguage} is fun: ${isFun} \x1b[33mMaybe\x1b[0m `);
+
+//10
+let and = 'and';
+console.log( programmingLanguage[0]  +" " + and + " "+ programmingLanguage[4] );
+
+// 11
+console.log(programmingLanguage.length);
