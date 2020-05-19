0f36f51916b8a117adcb56ac9baf4519d2ab0035
putkara <natia.meskhi.natia@gmail.com>
2020-05-19 18:51:54 +0200

    natia dan 9

---
+++ 09-PB-language-number/09natia.js
@@ -0,0 +1,29 @@
+
+//1
+let introSentence = 'hello, my name is Natia and I am';
+let age = 33;
+console.log(`${introSentence} ${age}`); 
+
+
+
+console.log(introSentence + ' ' + age); 
+// i like to do like in  1 style. `+` brings togetha 
+
+//2
+let c = 1005;
+c = parseInt(c);
+console.log(parseFloat(c));
+
+let d = 10.05;
+d = parseInt(d);
+console.log(parseFloat(d));
+
+//3
+let number = 30;
+console.log((number % 2) == 0 ? number + ' is even' : number + ' is odd');
+
+number = 939;
+console.log((number % 2) == 0 ? number + ' is even' : number + ' is odd');
+
+number = 40.9;
+console.log((number % 2) == 0 ? number + ' is even' : number + ' is odd');
