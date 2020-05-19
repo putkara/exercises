d02075821b7f9cdb5d494dd8ba02f8f3ed74ed10
putkara <natia.meskhi.natia@gmail.com>
2020-05-19 16:47:19 +0200

    natia

---
+++ 06-PB-ternary-exercises/06natia.js
@@ -0,0 +1,19 @@
+//1
+let isDog = null;
+console.log( isDog ?  'pat,pat' : 'find me a dog to pat!');
+
+//2
+let speedCheck = 60;
+console.log(speedCheck < 50 ?  "You're going to fast!" : "You're driving below the speed limit, Oma!");
+
+//3
+let age = 16;
+console.log(age > 18 ? "serve butter beer" : "serve beer");
+
+//4
+let isStudent = true;
+console.log(isStudent ? "Ticket costs €5,00" : "Ticket costs €12,00");
+
+//5
+let okMarie = 'cake';
+console.log(okMarie == 'cake'? "Let them eat cake" : "Oh, bother")
