a568ead8bdd8bdc6a74a9771a0806ed5005aeb59
putkara <natia.meskhi.natia@gmail.com>
2020-06-08 14:31:47 +0200

    26 solution

---
+++ 26-PB-function-weekend/26natia.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+const isWeekend = function (date1) {
+   const week = new Date(date1);
+
+   if (week.getDay() === 6 || week.getDay() === 0) {
+      console.log("It's weekend!!");
+   } else {
+      console.log("It's not weekend");
+   }
+}
+
+isWeekend('2020-2-11');
