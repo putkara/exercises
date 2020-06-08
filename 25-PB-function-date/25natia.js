384d8dc93727b552b85ed2439670790b054de2ac
putkara <natia.meskhi.natia@gmail.com>
2020-06-08 12:32:29 +0200

    25 solution

---
+++ 25-PB-function-date/25natia.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+const currentDay = new Date();
+const eventDay = new Date(2021, 2, 11);
+
+const bookEventDay = function (date, bookedDay)  {
+   if (date > bookedDay){ 
+   console.log(`We don't have tickets`);
+} else {
+    console.log(`We  have tickets ${bookedDay}`);
+    
+}
+ }
+
+ bookEventDay(currentDay, eventDay);
