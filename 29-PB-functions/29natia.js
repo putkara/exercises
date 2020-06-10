7ae3d9e4bf91498377afbc794e6e6c5e27a3b91b
putkara <natia.meskhi.natia@gmail.com>
2020-06-10 11:08:04 +0200

    change 29

--- 29-PB-functions/29natia.js
+++ 29-PB-functions/29natia.js
@@ -1,5 +1,5 @@
 //1
-
+console.log('1----------------------------');
 const answer = numbers => {
     let result = 0
     for (let i = 1; i <= numbers; i++) {
@@ -10,15 +10,15 @@ const answer = numbers => {
 };
 
 console.log(answer(4));
-
+console.log('7----------------------------');
 //7
 function calculateDogAge(humanAge, dogAge) {
-    return `Your doggie is ${dogAge * humanAge} years old in dog years !`;
+    return `Your dog is ${dogAge * humanAge} years old in dog years !`;
 }
 
 console.log(calculateDogAge(7, 5));
 
-
+console.log('3----------------------------');
 //3
 
 const whether = (word) => {
@@ -32,8 +32,9 @@ const whether = (word) => {
 console.log(whether("pastry"))
 console.log(whether("button"))
 
+console.log('4----------------------------');
+//4 
 
-//4
 function m(number) {
     if (number <= 0) {
         return true;
@@ -46,26 +47,52 @@ console.log(m(-1));
 console.log(m(5));
 
 
+console.log('5----------------------------')
 //5
-function countOccurrences(str, letter) 
-{
- let letterCount = 0;
- for (let position = 0; position < str.length; position++) 
- {
-    if (str.charAt(position) === letter) 
-      {
-      letterCount += 1;
-      }
-  }
-  return letterCount;
+function countOccurrences(str, letter) {
+    let letterCount = 0;
+    for (let position = 0; position < str.length; position++) {
+        if (str.charAt(position) === letter) {
+            letterCount += 1;
+        }
+    }
+    return letterCount;
 }
 
 console.log(countOccurrences("this is a string", 'i'));
 
-
+console.log('6----------------------------');
 //6
 console.log(Math.pow(5, 5));
 console.log(Math.pow(10, 10));
 console.log(Math.pow(3, 3));
 
 
+const power = function (base, exponent) {
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result *= base;
+    }
+    return result;
+};
+
+console.log(power(5, 5));
+console.log(power(10, 10));
+console.log(power(3, 3));
+
+//8
+
+
+console.log('8----------------------------');
+
+function calcSuppyCorrect(years, amount) {
+    const maxAge = 80;
+    if (years > maxAge) {
+        return "Too old";
+    }
+    leftYears = maxAge - years;
+    return leftYears * 365 * amount;
+}
+
+console.log(calcSuppyCorrect(25, 2));
+console.log(calcSuppyCorrect(40, 3));
