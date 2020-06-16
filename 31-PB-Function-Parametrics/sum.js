18671b47850ce04bea0809006b235cf572811cc9
putkara <natia.meskhi.natia@gmail.com>
2020-06-16 11:34:13 +0200

    sum.js

--- 31-PB-Function-Parametrics/sum.js
+++ 31-PB-Function-Parametrics/sum.js
@@ -1,6 +1,21 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
 
-const sum = () => {};
+
+
+
+
+const sum = (...elmt) => {
+    let sum1 = 0;
+for( let i = 0; i < elmt.length; i++ ){
+    sum1 +=  elmt[i]; 
+}
+
+return sum1;
+};
+
+
+
+
 
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
