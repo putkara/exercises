675f11de9f4833bb5b75be217d2adef06c87992b
putkara <natia.meskhi.natia@gmail.com>
2020-06-22 12:26:20 +0200

    35 2

---
+++ 35-PB-arrays-pt3/35natia.js
@@ -0,0 +1,23 @@
+function isFourLetters(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].length == 4) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+console.log(isFourLetters(["John", "James", "Jack", "Jeanne"]));
+console.log(isFourLetters(["Tomato", "Corn", "Lettuce"]));
+console.log(isFourLetters(["Dog", "Cat", "Deer"]));
+//2
+function month(num) {
+    let mon = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return mon[num - 1];
+}
+
+
+console.log(month(3));
+console.log(month(12));
+console.log(month(6));
+console.log(month(1));// my month
