31271d3603b78752e2b8fcfb0ed17d1a618f8d1c
putkara <natia.meskhi.natia@gmail.com>
2020-07-02 15:30:29 +0200

    45 2

--- 45-PB-datastructure-classes/45natia.js
+++ 45-PB-datastructure-classes/45natia.js
@@ -1,12 +1,33 @@
+///#### 1. Person Class
 class Person {
-    constructor(name, age) { 
-    this.name = name;
-    this.age = age;
-}
- }
-Person.prototype.greet = function() {
-    return `${this.name} ${this.age} years old.`;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.greet = function () {
+            return `${this.name} is ${this.age} years old.`;
+        }
+    }
+
 }
 
-const john = new Person('John,', 19); 
+const john = new Person('John,', 19);
 console.log(john.greet())
+
+
+///////////////////#### 2. Volume
+class Cylinder { 
+   constructor (cylHeight, cylDiameter) {
+
+
+    this.cylHeight = cylHeight;
+    this.cylDiameter = cylDiameter;
+}
+
+ Volume = function () {
+    let radius = this.cylDiameter / 2;
+    return this.cylHeight * Math.PI * radius * radius;
+     }
+};
+
+let cyl = new Cylinder(8, 4);
+console.log('volume =', cyl.Volume().toFixed(4));
